25a1d3238d11e1b109fc2206804d9302ab466c44
Lucas Scarano <scaranolucas75@gmail.com>
2022-04-08 20:46:48 -0300

    Update ejercicio_08.py

--- practico_01/ejercicio_08.py
+++ practico_01/ejercicio_08.py
@@ -9,13 +9,19 @@ def superposicion_basico(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool
 
     Restricción: Utilizar bucles anidados.
     """
-    pass # Completar
+    for ele1 in lista_1:
+        for ele2 in lista_2:
+            if ele1 == ele2:
+                return True
+    return False
 
 
 # NO MODIFICAR - INICIO
 test_list = [1, "hello", 35.20]
 assert superposicion_basico(test_list, (2, "world", 35.20))
 assert not superposicion_basico(test_list, (2, "world", 30.85))
+
+
 # NO MODIFICAR - FIN
 
 
@@ -24,13 +30,19 @@ assert not superposicion_basico(test_list, (2, "world", 30.85))
 
 def superposicion_in(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
     """Re-Escribir utilizando un sólo bucle y el operador IN."""
-    pass # Completar
+    for ele in lista_1:
+        if ele in lista_2:
+            return True
+    return False
+    pass  # Completar
 
 
 # NO MODIFICAR - INICIO
 test_list = [1, "hello", 35.20]
 assert superposicion_in(test_list, (2, "world", 35.20))
 assert not superposicion_in(test_list, (2, "world", 30.85))
+
+
 # NO MODIFICAR - FIN
 
 
@@ -41,13 +53,16 @@ def superposicion_any(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
     """Re-Escribir utilizando sin bucles, el operador in y la funcion any.
     Referencia: https://docs.python.org/3/library/functions.html#any
     """
-    pass # Completar
+    intersec = [value for value in lista_1 if value in lista_2]
+    return any(intersec)
 
 
 # NO MODIFICAR - INICIO
 test_list = [1, "hello", 35.20]
 assert superposicion_any(test_list, (2, "world", 35.20))
 assert not superposicion_any(test_list, (2, "world", 30.85))
+
+
 # NO MODIFICAR - FIN
 
 
@@ -58,7 +73,10 @@ def superposicion_set(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
     """Re-Escribir utilizando conjuntos (sets).
     Referencia: https://docs.python.org/3/library/stdtypes.html#set-types-set-frozenset
     """
-    pass # Completar
+    if set(lista_1).intersection(set(lista_2)):
+        return True
+    return False
+    pass  # Completar
 
 
 # NO MODIFICAR - INICIO
