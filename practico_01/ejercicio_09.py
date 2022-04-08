60498d843ac59421f3fcf6a702e55f7fc23b8f33
Lucas Scarano <scaranolucas75@gmail.com>
2022-04-08 20:54:56 -0300

    Update ejercicio_09.py
    Faltan Challenges opcionales

--- practico_01/ejercicio_09.py
+++ practico_01/ejercicio_09.py
@@ -6,12 +6,17 @@ def sumatoria_basico(n: int) -> int:
 
     Restricción: Utilizar un bucle for.
     """
-    pass # Completar
+    suma = 0
+    for i in range(n+1):
+        suma += i
+    return suma
 
 
 # NO MODIFICAR - INICIO
 assert sumatoria_basico(1) == 1
 assert sumatoria_basico(100) == 5050
+
+
 # NO MODIFICAR - FIN
 
 
@@ -22,7 +27,8 @@ def sumatoria_sum(n: int) -> int:
     """Re-Escribir utilizando la función sum y sin usar bucles.
     Referencia: https://docs.python.org/3/library/functions.html#sum
     """
-    pass # Completar
+    return sum(range(n+1))
+    pass  # Completar
 
 
 # NO MODIFICAR - INICIO
@@ -41,13 +47,15 @@ def sumatoria_reduce(n: int) -> int:
     """CHALLENGE OPCIONAL: Re-escribir utilizando reduce.
     Referencia: https://docs.python.org/3/library/functools.html#functools.reduce
     """
-    pass # Completar
+    pass  # Completar
 
 
 # NO MODIFICAR - INICIO
 if __name__ == "__main__":
     assert sumatoria_reduce(1) == 1
     assert sumatoria_reduce(100) == 5050
+
+
 # NO MODIFICAR - FIN
 
 
@@ -58,7 +66,7 @@ def sumatoria_gauss(n: int) -> int:
     """CHALLENGE OPCIONAL: Re-Escribir utilizando suma de Gauss.
     Referencia: https://es.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
     """
-    pass # Completar
+    pass  # Completar
 
 
 # NO MODIFICAR - INICIO
