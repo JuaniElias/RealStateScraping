0200a7fada2a2d27a64a683c1bda42cf53a7d4c5
Lucas Scarano <scaranolucas75@gmail.com>
2022-05-30 18:19:23 -0300

    Update ejercicio_07.py

--- practico_03/ejercicio_07.py
+++ practico_03/ejercicio_07.py
@@ -23,8 +23,10 @@ def actualizar_precio(articulos: List[Articulo], porcentaje: float) -> List[Arti
     Restricción: NO se debe modificar la clase ni los tests.
     Hint: Usar deepcopy (https://docs.python.org/3/library/copy.html#copy.deepcopy)
     """
-    
-    # Completar
+    arts_desc = deepcopy(articulos)
+    for arts in arts_desc:
+        arts.precio *= 1 + porcentaje/100
+    return arts_desc
 
 
 # NO MODIFICAR - INICIO
