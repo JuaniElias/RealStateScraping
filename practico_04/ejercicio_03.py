746a019040b2b6361955e731f4e2873193f73f7f
Lucas Scarano <scaranolucas75@gmail.com>
2022-06-05 16:55:49 -0300

    Update ejercicio_03.py

--- practico_04/ejercicio_03.py
+++ practico_04/ejercicio_03.py
@@ -1,6 +1,7 @@
 """Base de Datos SQL - Baja"""
 
 import datetime
+import sqlite3
 
 from practico_04.ejercicio_01 import reset_tabla
 from practico_04.ejercicio_02 import agregar_persona
@@ -10,13 +11,22 @@ def borrar_persona(id_persona):
     """Implementar la funcion borrar_persona, que elimina un registro en la 
     tabla Persona. Devuelve un booleano en base a si encontro el registro y lo 
     borro o no."""
-    pass # Completar
+    conn = sqlite3.connect('example.db')
+    cursor = conn.cursor()
+    sql_delete = f"delete from persona where persona.idpersona = {id_persona};"
+    cursor.execute(sql_delete)
+    filas_modificadas = cursor.rowcount
+    conn.commit()
+    conn.close()
+    return True if filas_modificadas > 0 else False
+
 
 # NO MODIFICAR - INICIO
 @reset_tabla
 def pruebas():
     assert borrar_persona(agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180))
     assert borrar_persona(12345) is False
+
 
 if __name__ == '__main__':
     pruebas()
