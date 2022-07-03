ad6a7be6cd234b4e259d92be4cd11565f49e1483
Lucas Scarano <scaranolucas75@gmail.com>
2022-07-03 19:04:16 -0300

    update ejercicio_01

--- practico_05/ejercicio_01.py
+++ practico_05/ejercicio_01.py
@@ -1,19 +1,21 @@
 """Base de Datos - Creación de Clase en ORM"""
 
-
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class Socio(Base):
-    """Implementar un modelo Socio a traves de Alchemy que cuente con los siguientes campos:
-        - id_socio: entero (clave primaria, auto-incremental, unico)
-        - dni: entero (unico)
+    """Implementar un modelo Socio a través de Alchemy que cuente con los siguientes campos:
+        - id_socio: entero (clave primaria, auto-incremental, único)
+        - dni: entero (único)
         - nombre: string (longitud 250)
         - apellido: string (longitud 250)
     """
     __tablename__ = 'socios'
-
-    # Completar
+    id = Column("id_socio", Integer, primary_key=True)
+    dni = Column(Integer)
+    nombre = Column(String(250))
+    apellido = Column(String(250))
 
--- practico_05/ejercicio_02.py
+++ practico_05/ejercicio_02.py
@@ -6,53 +6,53 @@ from ejercicio_01 import Base, Socio
 
 from typing import List, Optional
 
+
 class DatosSocio():
 
     def __init__(self):
-        pass # Completar
+        pass  # Completar
 
     def buscar(self, id_socio: int) -> Optional[Socio]:
         """Devuelve la instancia del socio, dado su id. Devuelve None si no 
         encuentra nada.
         """
-        pass # Completar
+        pass  # Completar
 
     def buscar_dni(self, dni_socio: int) -> Optional[Socio]:
         """Devuelve la instancia del socio, dado su dni. Devuelve None si no 
         encuentra nada.
         """
-        pass # Completar
-        
+        pass  # Completar
+
     def todos(self) -> List[Socio]:
         """Devuelve listado de todos los socios en la base de datos."""
-        pass # Completar
+        pass  # Completar
 
     def borrar_todos(self) -> bool:
         """Borra todos los socios de la base de datos. Devuelve True si el 
         borrado fue exitoso.
         """
-        pass # Completar
+        pass  # Completar
 
     def alta(self, socio: Socio) -> Socio:
         """Agrega un nuevo socio a la tabla y lo devuelve"""
-        pass # Completar
+        pass  # Completar
 
     def baja(self, id_socio: int) -> bool:
         """Borra el socio especificado por el id. Devuelve True si el borrado 
         fue exitoso.
         """
-        pass # Completar
+        pass  # Completar
 
     def modificacion(self, socio: Socio) -> Socio:
         """Guarda un socio con sus datos modificados. Devuelve el Socio 
         modificado.
         """
-        pass # Completar
-    
-    def contarSocios(self) -> int:
-        """Devuelve el total de socios que existen en la tabla"""
-        pass # Completar
+        pass  # Completar
 
+    def contar_socios(self) -> int:
+        """Devuelve el total de socios que existen en la tabla"""
+        pass  # Completar
 
 
 # NO MODIFICAR - INICIO
@@ -65,7 +65,7 @@ socio = datos.alta(Socio(dni=12345678, nombre='Juan', apellido='Perez'))
 assert socio.id > 0
 
 # Test Baja
-assert datos.baja(socio.id) == True
+assert datos.baja(socio.id)
 
 # Test Consulta
 socio_2 = datos.alta(Socio(dni=12345679, nombre='Carlos', apellido='Perez'))
