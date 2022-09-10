465d30e65e807417cd7c1ecb3c81b2207364a1aa
Lucas Scarano <scaranolucas75@gmail.com>
2022-09-10 18:39:19 -0300

    Parte 2 del tutorial de django completa

--- practico_07/view_practico_07/alta/models.py
+++ practico_07/view_practico_07/alta/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Socio(models.Model):
+    def __str__(self):
+        nombre_completo = self.nombre + " " + self.apellido
+        return nombre_completo
     id_socio = models.IntegerField(primary_key=True)
     dni = models.IntegerField(unique=True, null=False)
     nombre = models.CharField(max_length=250, null=False)
