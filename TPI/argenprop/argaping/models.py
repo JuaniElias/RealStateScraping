05961370e35f396bcd02d6e267f0a69b6e2709fb
Lucas Scarano <scaranolucas75@gmail.com>
2022-11-28 23:28:39 -0300

    Clases (models) creada y DB tambien

--- TPI/argenprop/argaping/models.py
+++ TPI/argenprop/argaping/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Barrio(models.Model):
+    id_barrio = models.IntegerField(primary_key=True)
+    nombre = models.CharField(null=False, max_length=250)
+
+class Propiedad(models.Model):
+    id_prop = models.IntegerField(primary_key=True)
+    precio = models.IntegerField(null=False)
+    calle = models.CharField(max_length=250)
+    nro = models.CharField(max_length=6)
+    barrio = models.ForeignKey(Barrio, on_delete=models.CASCADE)
+
+class Filtro(models.Model):
+    id_filtro = models.IntegerField(primary_key=True)
+    nombre = models.CharField(null=False, max_length=250)
--- TPI/argenprop/argenprop/settings.py
+++ TPI/argenprop/argenprop/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'argaping.apps.ArgapingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
