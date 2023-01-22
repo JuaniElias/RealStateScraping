fd8ff7cc1c77979660c076657231f2f4d3837564
Lucas Scarano <69877425+lucasscarano@users.noreply.github.com>
2023-01-21 21:23:37 -0300

    progress

--- TPI/argenprop/argaping/models.py
+++ TPI/argenprop/argaping/models.py
@@ -6,14 +6,22 @@ from django.db import models
 class Barrio(models.Model):
     id_barrio = models.IntegerField(primary_key=True)
     nombre = models.CharField(null=False, max_length=250)
+    def __str__(self):
+        return self.nombre
 
 class Propiedad(models.Model):
+    class Meta:
+        verbose_name_plural = 'Propiedades'
     id_prop = models.IntegerField(primary_key=True)
     precio = models.IntegerField(null=False)
     calle = models.CharField(max_length=250)
     nro = models.CharField(max_length=6)
     barrio = models.ForeignKey(Barrio, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.calle + " " + self.nro
 
 class Filtro(models.Model):
     id_filtro = models.IntegerField(primary_key=True)
     nombre = models.CharField(null=False, max_length=250)
+    def __str__(self):
+        return self.nombre
