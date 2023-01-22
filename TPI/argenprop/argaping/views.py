8e281c88c63d71ffd2ee0357977e32e7e0e741ef
Lucas Scarano <69877425+lucasscarano@users.noreply.github.com>
2023-01-22 20:09:17 -0300

    views_working

--- TPI/argenprop/argaping/views.py
+++ TPI/argenprop/argaping/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-
-    return HttpResponse("Hello, world. You're at the gaping index.")
+    template = loader.get_template('base.html')
+    return HttpResponse(template.render())
--- TPI/argenprop/argenprop/settings.py
+++ TPI/argenprop/argenprop/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gallery',
-    'bootstrap5',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +56,7 @@ ROOT_URLCONF = 'argenprop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
