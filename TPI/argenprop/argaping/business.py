80cc72a21561bbde1f8211e8dac2070e745dba7e
Lucas Scarano <69877425+lucasscarano@users.noreply.github.com>
2023-01-23 21:01:51 -0300

    avanzando_db

--- TPI/argenprop/argaping/business.py
+++ TPI/argenprop/argaping/business.py
@@ -1,19 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
-from argaping.models import Propiedad
+from argaping.models import Propiedad, Barrio
 
-url = "https://www.argenprop.com/departamento-alquiler-partido-rosario"
-# Manda la GET request
-f = requests.get(url=url)
-# Parsing
-doc = BeautifulSoup(f.text, "html.parser")
-# FindAll
-info_propiedades = doc.findAll("div", {"class": "card__monetary-values"})
+def load_db():
+    url = "https://www.argenprop.com/departamento-alquiler-partido-rosario"
+    # Manda la GET request
+    f = requests.get(url=url)
+    # Parsing
+    doc = BeautifulSoup(f.text, "html.parser")
+    # FindAll
+    info_propiedades = doc.findAll("div", {"class": "card__monetary-values"})
 
-for info in info_propiedades:
-    precio = info.find("span", {"class": "card__currency"}).next_sibling.text.strip()
-    direccion = info.find("h2", {"class": "card__address"}).next_element.strip().split(",", 1)[0]
-    moneda = "ARS" if info.find("span", {"class": "card__currency"}).next_element.strip() == "$" \
-                    else info.find("span", {"class": "card__currency"}).next_element.strip()
-    barrio = info.find("p", {"class": "card__title--primary show-mobile"}).next_element.strip().split(",", 1)[0]
-    Propiedad.objects.create(precio=precio, direccion=direccion, barrio=barrio, moneda=moneda)
+    for info in info_propiedades:
+        precio = info.find("span", {"class": "card__currency"}).next_sibling.text.strip()
+        direccion = info.find("h2", {"class": "card__address"}).next_element.strip().split(",", 1)[0]
+        moneda = "ARS" if info.find("span", {"class": "card__currency"}).next_element.strip() == "$" \
+                        else info.find("span", {"class": "card__currency"}).next_element.strip()
+        barrio = info.find("p", {"class": "card__title--primary show-mobile"}).next_element.strip().split(",", 1)[0]
+        #query = """SELECT nombre from argaping_barrio where nombre = {barrio} """ hay que ver como e esto
+        #nos quedamos acá viendo como hacer para buscar si existe el barrio ya, si no existe hay que crearlo
+        #falta también poner a barrio para que aparezca bien formateado: primera letra mayus, despues minus
+        Propiedad.objects.create(precio=precio, direccion=direccion, barrio=barrio, moneda=moneda)
--- TPI/argenprop/argaping/views.py
+++ TPI/argenprop/argaping/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from argaping.business import load_db
 
 # Create your views here.
 def index(request):
     template = loader.get_template('base.html')
     return HttpResponse(template.render())
+
+
+def request_page(request):
+    if(request.GET.get('btnBuscar')):
+        load_db()
+    return render(request,'load_data.html')
+
+"""def load_data(request):
+    template = loader.get_template('load_data.html')
+    return HttpResponse(template.render())"""
