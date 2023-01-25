edbc42405b75288a1f840b51aa6444c8048ad0bc
Juan Ignacio Elias <Juanignacioelias@hotmail.com>
2023-01-25 20:09:36 -0300

    venv viejo borrado

--- TPI/argenprop/argaping/models.py
+++ TPI/argenprop/argaping/models.py
@@ -6,22 +6,28 @@ from django.db import models
 class Barrio(models.Model):
     id_barrio = models.IntegerField(primary_key=True)
     nombre = models.CharField(null=False, max_length=250)
+
     def __str__(self):
         return self.nombre
+
 
 class Propiedad(models.Model):
     class Meta:
         verbose_name_plural = 'Propiedades'
+
     id_prop = models.IntegerField(primary_key=True)
     direccion = models.CharField(max_length=250)
     barrio = models.ForeignKey(Barrio, on_delete=models.CASCADE)
     moneda = models.CharField(max_length=5)
     precio = models.IntegerField(null=False)
+
     def __str__(self):
         return self.calle + " " + self.nro
+
 
 class Filtro(models.Model):
     id_filtro = models.IntegerField(primary_key=True)
     nombre = models.CharField(null=False, max_length=250)
+
     def __str__(self):
         return self.nombre
