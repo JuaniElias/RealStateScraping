2eac482ad5cbe21fb9aa2788e600b25dd62b54fd
Juan Ignacio Elias <Juanignacioelias@hotmail.com>
2023-01-25 23:36:23 -0300

    Saco los ID's para que sean autogenerados y pongo que el nombre de los barrios sea :star2: UNIQUE :star2:

--- TPI/argenprop/argaping/models.py
+++ TPI/argenprop/argaping/models.py
@@ -4,8 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Barrio(models.Model):
-    id_barrio = models.IntegerField(primary_key=True)
-    nombre = models.CharField(null=False, max_length=250)
+    nombre = models.CharField(null=False, max_length=250, unique=True)
 
     def __str__(self):
         return self.nombre
@@ -15,7 +14,6 @@ class Propiedad(models.Model):
     class Meta:
         verbose_name_plural = 'Propiedades'
 
-    id_prop = models.IntegerField(primary_key=True)
     direccion = models.CharField(max_length=250)
     barrio = models.ForeignKey(Barrio, on_delete=models.CASCADE)
     moneda = models.CharField(max_length=5)
@@ -26,7 +24,6 @@ class Propiedad(models.Model):
 
 
 class Filtro(models.Model):
-    id_filtro = models.IntegerField(primary_key=True)
     nombre = models.CharField(null=False, max_length=250)
 
     def __str__(self):
