e799435704d88b2712aee4e282bada33ad1f7b00
Juan Ignacio Elias <Juanignacioelias@hotmail.com>
2023-01-27 19:37:45 -0300

    View simple mustra los barrios para probar las templates
    Probablemente se haga la logica en la view y la vista en si en la template

--- TPI/argenprop/argaping/urls.py
+++ TPI/argenprop/argaping/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'argaping'
 urlpatterns = [
     path('', views.index, name='index'),
     path('load/', views.request_page, name='request_page'),
--- TPI/argenprop/argaping/views.py
+++ TPI/argenprop/argaping/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-from argaping.business import load_db
+from argaping.business import load_db, test_load_barrio
+from argaping.models import Propiedad, Barrio
+
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('base.html')
-    return HttpResponse(template.render())
+    latest_barrio_list = Barrio.objects.all()
+    output = ', '.join([b.nombre for b in latest_barrio_list])
+    context = {'output': latest_barrio_list}
+    return render(request, 'argaping/load_data.html', context)
 
 
 def request_page(request):
-    if(request.GET.get('btnBuscar')):
-        load_db()
-    return render(request,'load_data.html')
+    if request.GET.get('btnBuscar'):
+        test_load_barrio()
+    return render(request, 'load_data.html')
+
 
 """def load_data(request):
     template = loader.get_template('load_data.html')
