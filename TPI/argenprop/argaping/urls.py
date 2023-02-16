cb0d1a03688d4949f60fcfe5859d7f648d3d8a5a
Juan Ignacio Elias <Juanignacioelias@hotmail.com>
2023-02-16 18:58:33 -0300

    I did the impossible
    Carque el geojson desde el js. Habia que subirlo al server primero

--- TPI/argenprop/argaping/urls.py
+++ TPI/argenprop/argaping/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from django.contrib.staticfiles.storage import staticfiles_storage
-from django.views.generic import RedirectView
-from . import views
+from django.views.static import serve
+from django.conf import settings
 
+from . import views
 
 app_name = 'argaping'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('Barrios_de_Rosario.geojson', serve, {
+        'document_root': settings.STATICFILES_DIRS[0] + '/argaping/data/',
+        'path': 'Barrios_de_Rosario.geojson'
+    }),
+
 ]
