0182e5bec600dd78dbf3c9fe9e809a82ec4698eb
Juan Ignacio Elias <Juanignacioelias@hotmail.com>
2023-02-27 14:32:01 -0300

    Cambiado el static root path

--- TPI/argenprop/argenprop/settings.py
+++ TPI/argenprop/argenprop/settings.py
@@ -112,7 +112,8 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'argaping', 'static'),
 ]
-STATIC_ROOT = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = 'static/'
 
 # Default primary key field type
