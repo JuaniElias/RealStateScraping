3d80c849868dfab426e99fb26a3e8866acb048d5
Juan Ignacio Elias <Juanignacioelias@hotmail.com>
2023-03-07 23:40:12 -0300

    Static root generic

--- argenprop/argenprop/settings.py
+++ argenprop/argenprop/settings.py
@@ -113,7 +113,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'argaping', 'static'),
 ]
 
-STATIC_ROOT = '/home/juani347/RealStateScraping/argenprop/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
